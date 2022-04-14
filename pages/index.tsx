e92bfd1d4d93bc7838902b7b5c29e9d3ce283d94
Miwtoo <miwyugioh@gmail.com>
2022-04-15 00:54:28 +0700

    feat: :sparkles: init emotion
    https://github.com/vercel/next.js/blob/canary/examples/with-emotion/pages/index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 const IndexPage = () => (
-  <>Hello Next.js</>
+  <div>
+    <h3>Wongsakorn Chugasem (@miwtoo)</h3>
+    <p>Devleloper@ODDS</p>
+    <a href="https://github.com/miwtoo">Github</a> <br />
+    <a href="https://www.facebook.com/miwtoo">Facebook</a> <br />
+</div>
 )
 
 export default IndexPage
---
+++ shared/styles.tsx
@@ -0,0 +1,15 @@
+import { css, Global } from '@emotion/react'
+
+export const globalStyles = (
+  <Global
+    styles={css`
+      html,
+      body {
+        margin: 0;
+        min-height: 100%;
+        font-family: Helvetica, Arial, sans-serif;
+        font-size: 24px;
+      }
+    `}
+  />
+)
